b3f45e2c6a0ecd8bd035ba3b7742891e5ed64f7c
leriou <yange1876@hotmail.com>
2018-08-09 15:17:08 +0800

    会员权益解绑新增来源,方便处理退款用户重新开通的问题

--- async/async_test.py
+++ async/async_test.py
@@ -1,7 +1,7 @@
 import asyncio
 
 @asyncio.coroutine
-def wget(host):
+def wget1(host):
     print('wget %s...' % host)
     connect = asyncio.open_connection(host, 80)
     reader, writer = yield from connect
@@ -10,6 +10,21 @@ def wget(host):
     yield from writer.drain()
     while True:
         line = yield from reader.readline()
+        if line == b'\r\n':
+            break
+        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+    # Ignore the body, close the socket
+    writer.close()
+
+async def wget(host):
+    print('wget %s...' % host)
+    connect = asyncio.open_connection(host, 80)
+    reader, writer = await connect
+    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+    writer.write(header.encode('utf-8'))
+    await writer.drain()
+    while True:
+        line = await reader.readline()
         if line == b'\r\n':
             break
         print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
