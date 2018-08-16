5f78754c7eb1183682bdc4a90fd5da0a94273ed3
leriou <yange1876@hotmail.com>
2018-08-16 17:35:03 +0800

    emm

--- async/async_test.py
+++ async/async_test.py
@@ -1,37 +1,40 @@
 import asyncio
 
-@asyncio.coroutine
-def wget1(host):
-    print('wget %s...' % host)
-    connect = asyncio.open_connection(host, 80)
-    reader, writer = yield from connect
-    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
-    writer.write(header.encode('utf-8'))
-    yield from writer.drain()
-    while True:
-        line = yield from reader.readline()
-        if line == b'\r\n':
-            break
-        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
-    # Ignore the body, close the socket
-    writer.close()
+class AsyncDemo:
+    @asyncio.coroutine
+    def wget1(self,host):
+        print('wget %s...' % host)
+        connect = asyncio.open_connection(host, 80)
+        reader, writer = yield from connect
+        header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+        writer.write(header.encode('utf-8'))
+        yield from writer.drain()
+        while True:
+            line = yield from reader.readline()
+            if line == b'\r\n':
+                break
+            print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+        # Ignore the body, close the socket
+        writer.close()
 
-async def wget(host):
-    print('wget %s...' % host)
-    connect = asyncio.open_connection(host, 80)
-    reader, writer = await connect
-    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
-    writer.write(header.encode('utf-8'))
-    await writer.drain()
-    while True:
-        line = await reader.readline()
-        if line == b'\r\n':
-            break
-        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
-    # Ignore the body, close the socket
-    writer.close()
+    @staticmethod
+    async def wget(host):
+        print('wget %s...' % host)
+        connect = asyncio.open_connection(host, 80)
+        reader, writer = await connect
+        header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+        writer.write(header.encode('utf-8'))
+        await writer.drain()
+        while True:
+            line = await reader.readline()
+            if line == b'\r\n':
+                break
+            print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+        # Ignore the body, close the socket
+        writer.close()
+
 
 loop = asyncio.get_event_loop()
-tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+tasks = [AsyncDemo.wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
