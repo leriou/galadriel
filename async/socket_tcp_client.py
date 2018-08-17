c3c37bda0a0ae693cf7de091efdab0dd96bad57f
leriou <yange1876@hotmail.com>
2018-08-17 15:43:13 +0800

    使用socket直接根据rstp协议操作redis

--- async/socket_tcp_client.py
+++ async/socket_tcp_client.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
-s.connect(('127.0.0.1', 9999))
-# 接收欢迎消息:
-# print(s.recv(1024).decode('utf-8'))
-for data in [b'ping',b'push msg mt1',b'push msg ac1', b'push msg ac5', b'set msg1 1',b'pop msg',b'pop msg']:
-    # 发送数据:
-    s.send(data)
-    print(s.recv(1024).decode('utf-8'))
-s.send(b'exit')
+s.connect(('10.113.1.231',7001))
+
+
+def rstp(s):
+    res = []
+    res.append("*"+str(len(s.split())))
+    for n in s.split():
+        res.append("$"+str(len(n)))
+        res.append(n)
+    return bytes("\r\n".join(res)+"\r\n",encoding="utf8")
+
+for data in ["set a 190","expire a 40","cluster info","cluster nodes","hgetall ads:place:6"]:
+    s.send(rstp(data))
+    print(rstp(data))
+    print(s.recv(4096).decode('utf-8'))
+    # time.sleep(1)
 s.close()
